aaf497fcfa3cb3af97c1c0355945e53f2d3b1fbc
Daren-Ken St-laurent <Daren511@hotmail.com>
2014-09-19 12:22:55 -0400

    Connection Singleton

--- ThroneWarsServer/Program.cs
+++ ThroneWarsServer/Program.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Oracle.DataAccess.Client;
 
 namespace ThroneWarsServer
 {
     class Program
     {
+
+        
         static void Main(string[] args)
         {
             
         }
+
+        
     }
 }
