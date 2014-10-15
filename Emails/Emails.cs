ae1b1a44723453b7c6a745d09affd82a5675b642
Daren-Ken St-laurent <daren511@hotmail.com>
2014-10-15 11:03:11 -0400

    push emails

---
+++ Emails/Emails.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+
+namespace Emails
+{
+    public static class Email
+    {
+        public static bool sendMail(string email,string subject,string )
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtpout.secureserver.net");
+
+                mail.From = new MailAddress("noreply@thronewars.ca");
+                mail.To.Add("5148089676@txt.bell.ca");
+                mail.Subject = "Test Mail";
+                mail.Body = "This is for testing SMTP mail from GMAIL";
+
+                SmtpServer.Port = 25;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("noreply@thronewars.ca", "ProjetDEC");
+                SmtpServer.EnableSsl = false;
+
+                SmtpServer.Send(mail);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return false;
+            }
+        }
+    }
+}
