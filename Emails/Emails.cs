33a869d77ea017451b17d39c225bd7ef5f5880e2
hcfranck <hc_throwdown@hotmail.com>
2014-10-28 13:40:51 -0400

    Final push pour confirm ?

--- Emails/Emails.cs
+++ Emails/Emails.cs
@@ -12,12 +12,12 @@ namespace Emails
             "Voici votre lien pour confirmer : www.thronewars.ca:8080/ConfirmAccount.aspx?User=";
         public static string SujetForgetPass = "Récupération de mot de passe - Throne Wars";
         public static string BodyForgetPass = "Veuillez réinitialiser votre mot de passe à partir de cette page" +
-                                        "www.thronewars.ca:8080/ConfirmAccount?User=";
+                                        "www.thronewars.ca:8080/ResetPassword.aspx?User=";
         public static string SujetForgetUser = "Répupération de nom d'utilisateur -Throne Wars";
         public static string BodyForgetUser = "Voici votre nom d'utilisateur :";
         public static string SubjectResetPass = "Changement de mot de passe -Throne Wars";
         public static string BodyResetPass = "Pour changer votre mot de passe, veuillez visiter" +
-                                " ce lien et suivre les indications www.thronewars.com/ResetPassword?User=";
+                                " ce lien et suivre les indications www.thronewars.com/ResetPassword.aspx?User=";
 
         public static bool sendMail(string email,string subject,string message)
         {
