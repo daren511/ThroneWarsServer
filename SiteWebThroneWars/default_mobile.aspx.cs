bfc2051eeb40622caa9390c869086d9cf9686166
hcfranck <hc_throwdown@hotmail.com>
2014-11-18 09:22:20 -0500

    Test pour mobile

--- SiteWebThroneWars/default_mobile.aspx.cs
+++ SiteWebThroneWars/default_mobile.aspx.cs
@@ -4,18 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using ControleBD;
 
 namespace SiteWebThroneWars
 {
-    public partial class Page_Principale : System.Web.UI.Page
+    public partial class default_mobile : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (Request.Browser.IsMobileDevice)
-            {
-                Response.Redirect("~/default_mobile.aspx");
-            }
+
         }
     }
 }
