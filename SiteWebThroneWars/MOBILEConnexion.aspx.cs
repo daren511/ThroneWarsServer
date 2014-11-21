93adf9462c888ee71fe7deb0dcfefa086822c814
hcfranck <hc_throwdown@hotmail.com>
2014-11-21 14:03:35 -0500

    Begin #2

--- SiteWebThroneWars/MOBILEConnexion.aspx.cs
+++ SiteWebThroneWars/MOBILEConnexion.aspx.cs
@@ -4,14 +4,148 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ControleBD;
+using Oracle.DataAccess.Client;
+using System.Data;
 
 namespace SiteWebThroneWars
 {
     public partial class MOBILEConnexion : System.Web.UI.Page
     {
+
         protected void Page_Load(object sender, EventArgs e)
         {
+        }
+        protected void Connexion_Click(object sender, EventArgs e)
+        {
+            //String pour le sweetalert
+            string text = "";
+            DataSet DS = new DataSet();
+            DataSet DSLeaderboard = new DataSet();
+            // Textbox non null
+            bool ok = VerifChamps();
 
+            bool Connecter = false;
+            bool siConfirmer = false;
+            // if Text box pas null
+            if (ok)
+            {
+                //Variable des textbox
+                string user = username.Text;
+                string pass = password.Text;
+
+                string passHash = Controle.hashPassword(pass, null, System.Security.Cryptography.SHA256.Create());
+                Connecter = Controle.userPassCorrespondant(user, passHash);
+                if (Connecter)
+                    siConfirmer = Controle.accountIsConfirmed(user);
+                else
+                {
+                    text = "Compte inexistant";
+                    // Pas connecté
+                    ScriptManager.RegisterStartupScript(this.Page, this.GetType(), "tmp", "<script type='text/javascript'>MessageBoxWarning(\"" + text + "\");</script>", false);
+                    ViderTB();
+                }
+                if (!siConfirmer && Connecter)
+                {
+                    text = "Veuillez visiter votre courriel pour confirmer votre compte";
+                    // Pas connecté
+                    ScriptManager.RegisterStartupScript(this.Page, this.GetType(), "tmp", "<script type='text/javascript'>MessageBoxWarning(\"" + text + "\");</script>", false);
+                    ViderTB();
+                }
+                if (Connecter)
+                {
+                    // ScriptManager.RegisterStartupScript(this.Page, this.GetType(), "tmp", "<script type='text/javascript'>MessageBoxReussi();</script>", false);
+
+                    //Prend le JID
+                    int JID = Controle.getJID(user);
+
+
+                    if (JID != 0)
+                    {
+                        //Si oui > Ramener la position du leaderboard
+
+                        DSLeaderboard = Controle.getLeaderboard(user);
+                        if (DSLeaderboard != null)
+                        {
+                            GV_Leaderboard.DataSource = DSLeaderboard;
+                            GV_Leaderboard.DataBind();
+                        }
+
+                        // Return stats des persos dans un DataSet
+                        DS = Controle.getStatsWEB(JID);
+                        if (DS != null)
+                        {
+                            GV_Stats.DataSource = DS;
+                            GV_Stats.DataBind();
+                        }
+                        ScriptManager.RegisterStartupScript(this.Page, this.GetType(), "tmp", "<script type='text/javascript'>changeVisibility();</script>", false);
+                        ViderTB();
+
+                    }
+                    else
+                    {
+                        text = "JID Invalide ";
+                        //Textbox vide erreur
+                        ScriptManager.RegisterStartupScript(this.Page, this.GetType(), "tmp", "<script type='text/javascript'>MessageBoxErreur(\"" + text + "\");</script>", false);
+                        ViderTB();
+                    }
+                }
+                else
+                {
+                    text = "Le nom d'utilisateur et le mot de passe n'étaient pas correspondant";
+                    // Pas connecté
+                    ScriptManager.RegisterStartupScript(this.Page, this.GetType(), "tmp", "<script type='text/javascript'>MessageBoxErreur(\"" + text + "\");</script>", false);
+                    ViderTB();
+                }
+            }
+
+            else
+            {
+                text = "Vous devez remplir tout les champs requis";
+                //Textbox vide erreur
+                ScriptManager.RegisterStartupScript(this.Page, this.GetType(), "tmp", "<script type='text/javascript'>MessageBoxErreur(\"" + text + "\");</script>", false);
+                ViderTB();
+            }
+
+        }
+        protected bool VerifChamps()
+        {
+            bool Valide = true;
+            if (string.IsNullOrWhiteSpace(username.Text))
+                Valide = false;
+            if (string.IsNullOrWhiteSpace(password.Text))
+                Valide = false;
+
+            return Valide;
+        }
+        protected void ViderTB()
+        {
+            username.Text = "";
+            password.Text = "";
+        }
+
+        protected void GV_Leaderboard_OnRowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.DataRow)
+            {
+                // do your stuffs here, for example if column risk is your third column:
+                if (e.Row.Cells[1].Text == username.Text.ToLower())
+                {
+                    e.Row.BackColor = System.Drawing.Color.PowderBlue;
+                }
+            }
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
