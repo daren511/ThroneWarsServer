be0df2dec214532a40b99542402f9a2e400ee58b
alexis20 <lalonde.alexis@hotmail.com>
2014-11-24 20:31:05 -0500

    lister items perso

--- ThroneWars/Assets/Scripts/Menu/onMainMenu.cs
+++ ThroneWars/Assets/Scripts/Menu/onMainMenu.cs
@@ -238,12 +238,13 @@ public class onMainMenu : MonoBehaviour
 
         GUILayout.BeginHorizontal();
 
-        GUI.DrawTexture(new Rect(125, 20, 20, 20), _atkTexture, ScaleMode.StretchToFill, true, 0.0f);
-        GUI.DrawTexture(new Rect(175, 20, 20, 20), _defTexture, ScaleMode.StretchToFill, true, 0.0f);
-        GUI.DrawTexture(new Rect(225, 20, 20, 20), _matkTexture, ScaleMode.StretchToFill, true, 0.0f);
-        GUI.DrawTexture(new Rect(275, 20, 20, 20), _mdefTexture, ScaleMode.StretchToFill, true, 0.0f);
+        GUI.DrawTexture(new Rect(rectItem.width - 215, 20, 20, 20), _atkTexture, ScaleMode.StretchToFill, true, 0.0f);
+        GUI.DrawTexture(new Rect(rectItem.width - 165, 20, 20, 20), _defTexture, ScaleMode.StretchToFill, true, 0.0f);
+        GUI.DrawTexture(new Rect(rectItem.width - 115, 20, 20, 20), _matkTexture, ScaleMode.StretchToFill, true, 0.0f);
+        GUI.DrawTexture(new Rect(rectItem.width - 65, 20, 20, 20), _mdefTexture, ScaleMode.StretchToFill, true, 0.0f);
 
         GUILayout.EndHorizontal();
+        scrollPos = GUILayout.BeginScrollView(scrollPos, ColoredGUISkin.Skin.verticalScrollbar, ColoredGUISkin.Skin.scrollView);
 
         if (PlayerManager._instance._selectedCharacter != null &&
             PlayerManager._instance._selectedCharacter._characterInventory._invent.Count > 0)
@@ -252,11 +253,11 @@ public class onMainMenu : MonoBehaviour
             {
                 int offset = i * 25;
                 EquipableItem item = PlayerManager._instance._selectedCharacter._characterInventory._invent[i];
-                Rect itemButton = new Rect(0, 20 + offset, 100, 20);
+                Rect itemButton = new Rect(0, 20 + offset, rectItem.width - 245, 30);
 
                 if (PlayerManager._instance._selectedCharacter._characterInventory._invent.Contains(item))
                 {
-                    if (GUILayout.Button(item._itemName, GUILayout.Height(30), GUILayout.Width(100)))
+                    if (GUI.Button(itemButton, item._itemName))
                     {
                         PlayerManager._instance.UnequipItem(item._itemID);
                         int index = PlayerManager._instance._playerInventory._equips.IndexOf(
@@ -269,13 +270,14 @@ public class onMainMenu : MonoBehaviour
                         PlayerManager._instance.LoadPersonnage(PlayerManager._instance.GetPersonnage());
                     }
 
-                    GUI.Label(new Rect(125, 20 + offset, 50, 20), item._bonusPhysAtk.ToString());
-                    GUI.Label(new Rect(175, 20 + offset, 50, 20), item._bonusPhysDef.ToString());
-                    GUI.Label(new Rect(225, 20 + offset, 50, 20), item._bonusMagicAtk.ToString());
-                    GUI.Label(new Rect(275, 20 + offset, 50, 20), item._bonusMagicDef.ToString());
+                    GUI.Label(new Rect(rectInvent.width - 235, 20 + offset, 50, 20), item._bonusPhysAtk.ToString());
+                    GUI.Label(new Rect(rectInvent.width - 185, 20 + offset, 50, 20), item._bonusPhysDef.ToString());
+                    GUI.Label(new Rect(rectInvent.width - 135, 20 + offset, 50, 20), item._bonusMagicAtk.ToString());
+                    GUI.Label(new Rect(rectInvent.width - 85, 20 + offset, 50, 20), item._bonusMagicDef.ToString());
                 }
             }
         }
+        GUILayout.EndScrollView();
     }
 
     void doStatsWindow(int windowID)
