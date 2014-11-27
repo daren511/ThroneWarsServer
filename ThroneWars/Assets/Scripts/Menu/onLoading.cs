9424537f864b2b97d7f6b289f53c7df1515d4811
chunksahoy <charles_hunter182@hotmail.com>
2014-11-27 16:11:40 -0500

    Essai 3+ matchmaking

--- ThroneWars/Assets/Scripts/Menu/onLoading.cs
+++ ThroneWars/Assets/Scripts/Menu/onLoading.cs
@@ -11,13 +11,16 @@ public class onLoading : MonoBehaviour
     private string message = "En attente d'un autre joueur...";
     private Rect rect = new Rect((Screen.width - 400) / 2, (Screen.height - 75) / 2, 400, 90);
     public static Thread thread;
-    private bool coroutineStarted = false;
+    private bool doneLoading = false;
+
+    void Awake()
+    {
+
+    }
     void Start()
     {
-        PlayerManager._instance.sck.Blocking = false;
-        //FindPlayer();
-        PlayerManager._instance.LookForPlayer();
-
+        FindPlayer();
+        //StartCoroutine(StartSearch());
     }
     void OnGUI()
     {
@@ -27,33 +30,32 @@ public class onLoading : MonoBehaviour
         GUI.Window(-10, rect, doDisplayMessage, title);
         if (GUI.Button(new Rect(Screen.width - 200, Screen.height - 50, 200, 50), "Annuler"))
             cancelLoading();
-    }
 
+        if(!PlayerManager._instance.isLoading && !doneLoading)
+        {
+            doneLoading = true;
+            Debug.Log("Ennemi affecté");
+            GameManager._instance._enemySide = PlayerManager._instance._playerSide == 1 ? 2 : 1;
+
+            PlayerManager._instance.PrepareGame();
+            Application.LoadLevel("placement");
+        }
+    }
     private void doDisplayMessage(int windowID)
     {
         GUILayout.Space(25);
         GUILayout.Label(message);
     }
-
     private void cancelLoading()
     {
         PlayerManager._instance.LoadPlayer();
         Application.LoadLevel("MainMenu");
     }
-
     private void FindPlayer()
     {
-        //thread = new Thread(new ThreadStart(PlayerManager._instance.LookForPlayer));
-        //thread.Start();
-
-        //if (!coroutineStarted)
-        //{
-        //    StartCoroutine(WaitForPlayer());
-        //    coroutineStarted = true;
-        //}
-
+        thread = new Thread(new ThreadStart(PlayerManager._instance.Lobby));
+        thread.Start();
     }
-
     IEnumerator WaitForPlayer()
     {
         while (PlayerManager._instance.isWaitingPlayer)
@@ -68,5 +70,15 @@ public class onLoading : MonoBehaviour
             Debug.Log("minute , je t'attends");
             yield return this;
         }
+    }
+    IEnumerator StartSearch()
+    {
+        //if (!started)
+        //{
+        //    started = true;
+        //    yield return new WaitForSeconds(1);
+        //    PlayerManager._instance.LookForPlayer();
+        //}
+        yield return 0;
     }
 } 
