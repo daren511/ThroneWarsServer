fe5e0c712f85359418e140629334404ed96b32bf
hcfranck <hc_throwdown@hotmail.com>
2014-12-02 09:00:16 -0500

    Encryption + Regex user only number/letter dans inscription

--- SiteWebThroneWars/Inscription.aspx.cs
+++ SiteWebThroneWars/Inscription.aspx.cs
@@ -25,6 +25,8 @@ namespace SiteWebThroneWars
             string text = "";
             // Si user est plus que 4 characteres
             bool userOK = false;
+            // Si user est de format valide
+            bool isValid = false;
             // Verif si all textbox sont pas vide
             bool ok = VerifChamps();
             if (ok)
@@ -43,13 +45,24 @@ namespace SiteWebThroneWars
                     ScriptManager.RegisterStartupScript(this.Page, this.GetType(), "tmp", "<script type='text/javascript'>MessageBoxErreur(\"" + text + "\");</script>", false);
                     ViderTB();
                 }
+                if (Regex.IsMatch(user, @"^[a-zA-Z0-9]+$"))
+                {
+                    isValid = true;
+                }
+                else
+                {
+                    text = "Le format de votre nom d'usager est invalide";
+                    ScriptManager.RegisterStartupScript(this.Page, this.GetType(), "tmp", "<script type='text/javascript'>MessageBoxErreur(\"" + text + "\");</script>", false);
+                    ViderTB();
+                }
+                
 
                 // Verifier si email est legit ou non vide
                 bool legitEmail = IsEmail(courriel);
 
 
                 // Verifier si mot de passe = confirmation && Email == confirmation && Email legit
-                if (password.Text == cpassword.Text && email.Text == cemail.Text && legitEmail && userOK)
+                if (password.Text == cpassword.Text && email.Text == cemail.Text && legitEmail && userOK && isValid)
                 {
                     bool InsReussi = false;
                     // Inserer dans oracle
