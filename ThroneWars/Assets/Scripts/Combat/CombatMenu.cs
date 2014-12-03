59dcfe2ce0e7f70e02974d3b1a14052bb38f186a
chunksahoy <charles_hunter182@hotmail.com>
2014-12-02 23:19:05 -0500

    test combat

--- ThroneWars/Assets/Scripts/Combat/CombatMenu.cs
+++ ThroneWars/Assets/Scripts/Combat/CombatMenu.cs
@@ -110,7 +110,7 @@ public class CombatMenu : MonoBehaviour
 
     void OnGUI()
     {
-        gameOver = true;
+        //gameOver = true;
         hasUpdatedGUI = ResourceManager.GetInstance.UpdateGUI(hasUpdatedGUI);
         if (!gameOver)
         {
